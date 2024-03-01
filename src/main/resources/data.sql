8f8105f5abd47c331d5eb0b81522a4ee15643892
decodingcloud <decodingcloud@gmail.com>
2024-03-01 02:03:03 -0600

    initializing data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Todo (title, completed) VALUES ('Learn Java', true);
+INSERT INTO Todo (title, completed) VALUES ('Learn AWS', false);
+INSERT INTO Todo (title, completed) VALUES ('Learn GCP', false);
+INSERT INTO Todo (title, completed) VALUES ('Learn Kubernetes', true);
